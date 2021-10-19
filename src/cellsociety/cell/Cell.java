573b9aff2680b8e5ecc5130e4554da99b50d8dcd
Tarun <tarunamasa@gmail.com>
2021-10-19 18:16:35 -0400

    Changed small logic within cell

--- src/cellsociety/cell/Cell.java
+++ src/cellsociety/cell/Cell.java
@@ -1,4 +1,4 @@
-package cellsociety.CellParts;
+package cellsociety.cell;
 
 import cellsociety.location.CornerLocation;
 
@@ -13,11 +13,12 @@ public class Cell {
     List<CornerLocation> corners;
     private int iIndex;
     private int jIndex;
+    private HashMap<Integer, Integer> neighborStateMap;
 
-    public Cell(int i, int j, int intialState){
+    public Cell(int i, int j, int initialState){
         this.iIndex = i;
         this.jIndex = j;
-        this.currentState = intialState;
+        this.currentState = initialState;
     }
 
 
@@ -28,7 +29,7 @@ public class Cell {
 
 
     public void setFutureState(int state){
-        this.futureState = futureState;
+        this.futureState = state;
     }
 
     public int getCurrentState() {
@@ -36,17 +37,35 @@ public class Cell {
     }
 
     //FIXME: Not complete
-    public List<Cell> getNeighbors(){
+    public void initializeNeighborList(){
 
-        ArrayList<Cell> neighborList = new ArrayList<>();
+        neighbors = new ArrayList<>();
+
 
         //FIXME: loop through the cellGrid somehow using corners, then do this next part:
-        Cell cell = new Cell(0,0,0);
-        if (!neighborList.contains(cell)){
-            neighborList.add(cell);
-        }
+//
+    Cell cell = new Cell(0, 0, 0);
+    if (!neighbors.contains(cell)) {
+        neighbors.add(cell);
+    }
+//
+        this.createNeighborStateMap();
+    }
 
-        return neighborList;
+    public int numOfStateNeighbors(int state){
+        if(neighborStateMap.containsKey(state)){
+            return neighborStateMap.get(state);
+        }
+        return 0;
+    }
+
+    public void createNeighborStateMap(){
+        neighborStateMap = new HashMap<>();
+        for(Cell neigbor: neighbors){
+           int state = neigbor.getCurrentState();
+            neighborStateMap.putIfAbsent(state, 0);
+            neighborStateMap.put(state, neighborStateMap.get(state)+1);
+        }
     }
 
 
@@ -54,18 +73,17 @@ public class Cell {
         this.cellGrid = cellGrid;
     }
 
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Cell)) return false;
+        if (o == null || getClass() != o.getClass()) return false;
         Cell cell = (Cell) o;
-        return currentState == cell.currentState && futureState == cell.futureState && iIndex == cell.iIndex && jIndex == cell.jIndex && getNeighbors().equals(cell.getNeighbors()) && Arrays.equals(cellGrid, cell.cellGrid) && corners.equals(cell.corners);
+        return currentState == cell.currentState && futureState == cell.futureState && iIndex == cell.iIndex && jIndex == cell.jIndex && Objects.equals(neighbors, cell.neighbors) && Arrays.equals(cellGrid, cell.cellGrid) && Objects.equals(corners, cell.corners) && Objects.equals(neighborStateMap, cell.neighborStateMap);
     }
 
     @Override
     public int hashCode() {
-        int result = Objects.hash(currentState, futureState, getNeighbors(), corners, iIndex, jIndex);
+        int result = Objects.hash(currentState, futureState, neighbors, corners, iIndex, jIndex, neighborStateMap);
         result = 31 * result + Arrays.hashCode(cellGrid);
         return result;
     }
