f48a0b5ce7012ca867557c25e79e923daf43d513
Evan Kenyon <ek168@duke.edu>
2021-10-19 23:14:49 -0400

    Added save file functionality

--- src/cellsociety/controller/Controller.java
+++ src/cellsociety/controller/Controller.java
@@ -1,20 +1,24 @@
 package cellsociety.controller;
 
 import cellsociety.Model.Model;
+import cellsociety.Utilities.CSVGenerator;
 import cellsociety.Utilities.CSVParser;
 import cellsociety.cell.Cell;
 import cellsociety.view.MainView;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Controller {
 
   private CSVParser parser;
+  private CSVGenerator generator;
   private Model model;
   private MainView mainView;
 
   public Controller() {
     this.parser = new CSVParser();
+    this.generator = new CSVGenerator();
     this.mainView = null;
     this.model = null;
   }
@@ -25,6 +29,11 @@ public class Controller {
     parser.initializeCellMatrix();
     Cell[][] cellGrid = parser.getCellMatrix();
     model.setCellGrid(cellGrid);
+  }
+
+  // TODO: actually handle exception
+  public void saveFile() throws IOException {
+    generator.createCSVFile(model.getCellGrid());
   }
 
   public void setMainView(MainView mainView) {
