a98ef76b78ded976b193096d4d10c9fcfa64c7a4
Evan Kenyon <ek168@duke.edu>
2021-10-26 21:52:04 -0400

    Replaced finding pathname with simpler implementation for SimParser

--- src/cellsociety/Utilities/SimParser.java
+++ src/cellsociety/Utilities/SimParser.java
@@ -28,11 +28,10 @@ public class SimParser {
   }
 
   // TODO: handle exception properly
-  public void setupKeyValuePairs(File csvFile) throws IOException, InputMismatchException {
+  public void setupKeyValuePairs(File simFile) throws IOException, InputMismatchException {
     // Used https://mkyong.com/java/java-read-a-file-from-resources-folder/ to learn how to properly
     // setup pathname
-    String pathName =
-        getRelativeParentDirectory(csvFile.getParent()) + csvFile.getName().replace(".csv", ".sim");
+    String pathName = String.format("%s/%s", simFile.getParentFile().getName(), simFile.getName());
     // Borrowed code for making InputStream from
     // https://www.baeldung.com/convert-file-to-input-stream
     InputStream simFileInputStream = this.getClass().getClassLoader().getResourceAsStream(pathName);
@@ -68,20 +67,5 @@ public class SimParser {
 
   public Properties getSimulationConfig() {
     return simulationConfig;
-  }
-
-  private String getRelativeParentDirectory(String absoluteDirectory) {
-    String[] allDirectories = absoluteDirectory.split("/");
-    String relativeDirectory = "";
-    boolean shouldAdd = false;
-    for (String directory : allDirectories) {
-      if (shouldAdd) {
-        relativeDirectory += directory + "/";
-      }
-      if (directory.equals("data")) {
-        shouldAdd = true;
-      }
-    }
-    return relativeDirectory;
   }
 }
